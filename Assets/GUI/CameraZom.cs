2ee6d5d694870c2391f67775ba8092c284394ae4
MSI-GE62 <AinoraZ@gmail.com>
2016-06-04 15:00:01 +0300

    Edves scriptai

---
+++ Assets/GUI/CameraZom.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraZom : MonoBehaviour {
+
+	public Camera cam;
+	public int zoomValue;
+
+	public int minSize;
+	public int maxSize;
+
+	public void Zoom () {
+		if(cam.orthographicSize + zoomValue >= minSize && cam.orthographicSize + zoomValue <= maxSize) cam.orthographicSize += zoomValue;
+	}
+}
