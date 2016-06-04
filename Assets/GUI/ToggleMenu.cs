088ff57ad28c6f630b8c7e0c1da733780524e95a
edve98 <edvinas.senda@gmail.com>
2016-06-04 13:50:46 +0300

    UI things

---
+++ Assets/GUI/ToggleMenu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToggleMenu : MonoBehaviour {
+
+	public GameObject menuObj;
+	public GameObject[] OtherMenus;
+
+	public void Toggle () {
+		for (int i = 0; i < OtherMenus.Length; i++) {
+			OtherMenus[i].SetActive(false);
+		}
+		menuObj.SetActive ( !menuObj.activeSelf );
+	}
+
+}
---
+++ Assets/GUI/spwncrd.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spwncrd : MonoBehaviour {
+
+	public CardManager manager;
+
+	public void spwn () {
+		
+		manager.AddCard (Random.Range(0,  3));
+	}
+}
