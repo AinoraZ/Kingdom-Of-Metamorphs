82f169298251442551f44f0d004b37eeba8f77cd
edve98 <edvinas.senda@gmail.com>
2016-06-05 14:59:50 +0300

    points and things (need minion info)

---
+++ Assets/GUI/GemCount.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GemCount : MonoBehaviour {
+
+	public int gems = 0;
+	public Text text;
+	public GameObject thing;
+
+	void Update () {
+		text.text = gems + " gems";
+	}
+
+	public void DoPoints () {
+		int addz = 2;
+		for(int i=0; i < Uti.ListLength(thing.GetComponent<MoveHandler> ().rePoints); i++){
+			if(thing.GetComponent<MoveHandler> ().rePoints[i].GetComponent<TileInfo>().minion != null){
+				if(thing.GetComponent<MoveHandler> ().rePoints[i].GetComponent<TileInfo>().minion.tag == "P1" && thing.GetComponent<MoveHandler> ().rePoints[i].name != "Friendly"){
+					addz += 2;
+				}
+			}
+		}
+		gems += addz;
+	}
+}
---
+++ Assets/GUI/SpawnMinion.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnMinion : MonoBehaviour {
+
+	public GameObject gemcount;
+	public GameObject camera;
+	public GameObject spawnThis;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void SpawnThis () {
+		camera.GetComponent<Spawner> ().friendlyMinion = spawnThis;
+		gemcount -= camera.GetComponent<Spawner> ().friendlyMinion;
+	}
+}
